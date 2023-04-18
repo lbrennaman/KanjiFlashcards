da86ec96d5de4eb283b11efc066b9236a546e119
Logan Brennaman <brennaman.4@wright.edu>
2023-04-18 13:06:24 -0400

    Updating base so that vite deploys correctly

--- KanjiFlashcards/vite.config.js
+++ KanjiFlashcards/vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const ASSET_URL = process.env.ASSET_URL || '';
-
-console.log("Directory: ", `${ASSET_URL}/dist/`);
-
 // https://vitejs.dev/config/
 // https://stackoverflow.com/questions/68076527/how-to-set-vite-config-js-base-public-path
 export default defineConfig({
   plugins: [react()],
-  base: `${ASSET_URL}/dist/`
+  base: '/KanjiFlashcards/'
 })
