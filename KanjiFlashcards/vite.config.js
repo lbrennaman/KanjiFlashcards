dc8d08a5dd205e227542c36c28b1403d50eb6fb9
Logan Brennaman <brennaman.4@wright.edu>
2023-04-18 13:04:02 -0400

    Update vite.config.js

--- KanjiFlashcards/vite.config.js
+++ KanjiFlashcards/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 const ASSET_URL = process.env.ASSET_URL || '';
 
+console.log("Directory: ", `${ASSET_URL}/dist/`);
+
 // https://vitejs.dev/config/
 // https://stackoverflow.com/questions/68076527/how-to-set-vite-config-js-base-public-path
 export default defineConfig({
